e3d95981103534794cb057c86e0b5062bf664f1d
KovalM <m.koval@mail.by>
2015-07-26 15:03:11 +0300

    full authorization view

--- article/views.py
+++ article/views.py
@@ -1,11 +1,19 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 from django.views.generic import FormView, TemplateView
 from registration.backends.default.views import RegistrationView
+from django.shortcuts import render_to_response
+from django.contrib import auth
 
+def main(request):
+    return render_to_response('base.html', {'username':auth.get_user(request).username})
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
 class RegisterFormView(RegistrationView):
     template_name = "registration.html"
@@ -14,7 +22,7 @@ class RegisterFormView(RegistrationView):
 class LoginFormView(FormView):
     form_class = AuthenticationForm
     template_name = "login.html"
-    success_url = "/hello"
+    success_url = "/"
 
     def form_valid(self, form):
         self.user = form.get_user()
