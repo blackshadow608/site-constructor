40daffe3eb7708a01fd56b963f79a3c1ecd54297
Your Name <you@example.com>
2015-07-27 13:40:15 +0300

    Clean db

--- article/admin.py
+++ article/admin.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-from article.models import Comments, Article, Project, PageProject
+from article.models import Project, PageProject
 
 # Register your models here.
-class ArticleInline(admin.StackedInline):
-    model = Comments
-    extra = 2
-
-class ArticleAdmin(admin.ModelAdmin):
-    fields = ['title','text','date']
-    inlines = [ArticleInline]
-    list_filter = ['date','title','likes']
 
 class ProjectInline(admin.StackedInline):
     model = PageProject
@@ -19,6 +11,5 @@ class ProjectAdmin(admin.ModelAdmin):
     fields = ['project_user','project_name']
     inlines = [ProjectInline]
 
-admin.site.register(Article, ArticleAdmin)
 admin.site.register(Project, ProjectAdmin)
 
