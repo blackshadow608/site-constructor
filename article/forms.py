709ac21f570630e90853035fb4b29e48ea3263ef
Your Name <you@example.com>
2015-08-01 16:23:02 +0300

    add video(bags)

--- article/forms.py
+++ article/forms.py
@@ -1,23 +1,5 @@
 from django import forms
-# from django.contrib.auth.models import User
-# from article.models import Project, PageProject
-#
-# __author__ = 'evgen'
-#
-# class UserForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ['username']
-#
-#  class ProjectForm(forms.ModelForm):
-#     class Meta:
-#         model = Project
-#         fields = ['username']
-#
-# class PageForm(forms.ModelForm):
-#     class Meta:
-#         model = PageProject
-#         fields = ['username']
+
 from django.forms import ModelForm
 from article.models import Project, PageProject
 
--- static/js/dragndrop.js
+++ static/js/dragndrop.js
@@ -1,12 +1,33 @@
     $(function() {
-        $(".draggable").draggable({
+        $(".draggable-video").draggable({
             helper: "clone", connectToSortable: "#droppable",
             start: function (event, ui) {
                 var clone = $(ui.helper);
                 clone.addClass("xyi");
+
+
             },
             stop: function (event, ui) {
-                $(ui.helper).css('width', '100%');
+                url=prompt('youtube.com link');
+                if(url) {
+                    console.log(url);
+                    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+                    var match = url.match(regExp);
+                    if (match && match[2].length == 11) {
+                        url = match[2];
+                        $(ui.helper).empty()
+                        $(ui.helper).append('<iframe width="560" height="315"  src="https://www.youtube.com/embed/' + url + '" frameborder="0" allowfullscreen></iframe>')
+                        $(ui.helper).removeClass('btn-primary')
+                        $(ui.helper).css('height', 'auto')
+                        $(ui.helper).css('width', '100%')
+                    } else {
+                        $(ui.helper).remove();
+                    }
+                }else{
+                    $(ui.helper).remove();
+                }
+
+
             }
         });
         $("#droppable").sortable({
