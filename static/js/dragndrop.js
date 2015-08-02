db3617f20fef3c084dd1167266f79ae69b9e36f4
Your Name <you@example.com>
2015-08-02 12:40:07 +0300

    sytylish video add

--- static/js/dragndrop.js
+++ static/js/dragndrop.js
@@ -6,24 +6,35 @@
                 clone.addClass("xyi");
             },
             stop: function (event, ui) {
-                url=prompt('youtube.com link');
-                if(url) {
-                    console.log(url);
-                    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-                    var match = url.match(regExp);
-                    if (match && match[2].length == 11) {
-                        url = match[2];
-                        $(ui.helper).empty();
-                        $(ui.helper).append('<iframe width="560" height="315"  src="https://www.youtube.com/embed/' + url + '" frameborder="0" allowfullscreen></iframe>');
-                        $(ui.helper).removeClass('btn-primary');
-                        $(ui.helper).css('height', 'auto');
-                        $(ui.helper).css('width', '100%');
-                    } else {
-                        $(ui.helper).remove();
+                var statesdemo = {
+                    state0: {
+                        title: 'Video block',
+                        html:'<label>Youtube link <input class="form-control" type="text" name="link" value=""></label><br />',
+                        buttons: {OK: 1, Cancel:false },
+
+                        submit:function(e,v,m,f){
+                            var url=(f.link);
+                            if(url) {
+                                var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+                                var match = url.match(regExp);
+                                if (match && match[2].length == 11) {
+                                    url = match[2];
+                                    $(ui.helper).empty();
+                                    $(ui.helper).append('<iframe width="560" height="315"  src="https://www.youtube.com/embed/' + url + '" frameborder="0" allowfullscreen></iframe>');
+                                    $(ui.helper).removeClass('btn-primary');
+                                    $(ui.helper).css('height', 'auto');
+                                    $(ui.helper).css('width', '100%');
+                                } else {
+                                    $(ui.helper).remove();
+                                }
+                            }else{
+                                $(ui.helper).remove();
+                            }
+                        },
+                        close: function(e,v,m,f){$(ui.helper).remove()}
                     }
-                }else{
-                    $(ui.helper).remove();
-                }
+                };
+                $.prompt( statesdemo);
             }
         });
         $(".draggable-text").draggable({
